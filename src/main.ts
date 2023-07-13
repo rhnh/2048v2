512c3517f43c9a316a270e7a85e61b753b1c687c
rhnh <rhnh@protonmail.com>
2023-07-13 19:11:56 +0200

    more colors

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,14 @@ import "./style/index.css"
 import { buttonBar, selectorBoard } from "./components"
 let cells = generate2DArray(4)
 cells = fillCells(cells, true)(2)
-cells = [
-  [16, 8, 2048, 1024],
-  [32, 32, 2, 4],
-  [512, 256, 512, 128],
-  [64, 32, 8, 4],
-]
+// cells = [
+//   [16, 8, 2048, 1024],
+//   [32, 32, 2, 4],
+//   [512, 256, 512, 128],
+//   [64, 32, 8, 4],
+// ]
 const board = createElement("article")("board")
+board.className += " o5 "
 renderBoard({ cells, board, state: "idle" })
 selectorBoard(board, cells)
 document.body.append(
